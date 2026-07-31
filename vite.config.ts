a3d9b869a71c7959ae03a5be1a8bdc1c4a6f4108
u8902054545-design <u8902054545@gmail.com>
2026-07-31 17:43:03 +0300

    refactor(vite): replace __dirname with import.meta.dirname

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig(({mode}) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(import.meta.dirname, '.'),
       },
     },
     server: {
